55f1a08fb185edd9dd21eb5ddbc644b0cc7f1483
brechen <zappeln@gmail.com>
2015-10-21 17:40:45 -0500

    simplified class methods

--- lib/record_list/list_display.rb
+++ lib/record_list/list_display.rb
@@ -1,27 +1,29 @@
 class ListDisplay
+  class << self
 
-  def self.html_display(data)
-    string_array = []
-    data.each do |line|
-      string_array << "#{line.last_name}, #{line.first_name}, #{line.gender}, #{line.fav_color}, #{line.birthdate}"
+    def html_display(data)
+      string_array = []
+      data.each do |line|
+        string_array << "#{line.last_name}, #{line.first_name}, #{line.gender}, #{line.fav_color}, #{line.birthdate}"
+      end
+      string_array
     end
-    string_array
-  end
 
-  def self.list_type_prompt
-    puts "Please choose listing type:"
-    puts "1) By gender"
-    puts "2) By birthdate"
-    puts "3) By lastname"
-  end
+    def list_type_prompt
+      puts "Please choose listing type:"
+      puts "1) By gender"
+      puts "2) By birthdate"
+      puts "3) By lastname"
+    end
 
-  def self.entry_error
-    puts "Sorry, but that was an invalid entry."
-  end
+    def entry_error
+      puts "Sorry, but that was an invalid entry."
+    end
 
-  def self.terminal_display(data)
-    data.each do |line|
-      puts "#{line.last_name}, #{line.first_name}, #{line.gender}, #{line.fav_color}, #{line.birthdate}"
+    def terminal_display(data)
+      data.each do |line|
+        puts "#{line.last_name}, #{line.first_name}, #{line.gender}, #{line.fav_color}, #{line.birthdate}"
+      end
     end
   end
 end
