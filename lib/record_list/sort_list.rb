91f3429bb002a1126f9f45db28a07bfbfbc6614c
brechen <zappeln@gmail.com>
2015-10-21 17:53:13 -0500

    simplified additional class methods

--- lib/record_list/sort_list.rb
+++ lib/record_list/sort_list.rb
@@ -1,24 +1,25 @@
 class SortList
+  class << self
 
-
-  def self.by_gender(records)
-    first = []
-    last = []
-    records.each do |record|
-      if record.gender == "Female"
-        first << record
-      else
-        last << record
+    def by_gender(records)
+      first = []
+      last = []
+      records.each do |record|
+        if record.gender == "Female"
+          first << record
+        else
+          last << record
+        end
       end
+      first + last
     end
-    first + last
-  end
 
-  def self.by_birthdate(records)
-    records.sort_by! {|record| record.birthdate}
-  end
+    def by_birthdate(records)
+      records.sort_by! {|record| record.birthdate}
+    end
 
-  def self.by_lastname(records)
-    records.sort_by! {|record| record.last_name}
+    def by_lastname(records)
+      records.sort_by! {|record| record.last_name}
+    end
   end
 end
