8df2f21d01f3eb8f6a5eaacf3c300d7adf103200
brechen <zappeln@gmail.com>
2015-11-09 22:58:19 -0600

    refactor varible names

--- lib/record_list/person.rb
+++ lib/record_list/person.rb
@@ -2,11 +2,11 @@ class Person
   attr_reader :last_name, :first_name, :gender, :fav_color
   attr_accessor :birthdate
 
-  def initialize(personel_data)
-    @last_name = personel_data[0]
-    @first_name = personel_data[1]
-    @gender = personel_data[2]
-    @fav_color = personel_data[3]
-    @birthdate = personel_data[4]
+  def initialize(record_data)
+    @last_name = record_data[0]
+    @first_name = record_data[1]
+    @gender = record_data[2]
+    @fav_color = record_data[3]
+    @birthdate = record_data[4]
   end
 end
--- lib/record_list/sort_list.rb
+++ lib/record_list/sort_list.rb
@@ -2,15 +2,15 @@ class SortList
   class << self
 
     def by_gender(records)
-      records.sort_by {|e| e.gender}
+      records.sort_by {|record| record.gender}
     end
 
     def by_birthdate(records)
-      records.sort_by! {|record| record.birthdate}
+      records.sort_by {|record| record.birthdate}
     end
 
     def by_lastname(records)
-      records.sort_by! {|record| record.last_name}
+      records.sort_by {|record| record.last_name}
     end
   end
 end
