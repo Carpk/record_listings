9b1c51ec896a77337837cdb7d56ff9662efd7e4b
brechen <zappeln@gmail.com>
2016-01-08 19:39:05 -0600

    refactor using template pattern

--- lib/record_list/record.rb
+++ lib/record_list/record.rb
@@ -1,8 +1,16 @@
 class Record
+  class << self
 
-  def self.load_files
-    parser = FileParser.new
-    parser.load_listed
+    def load_files
+      parser = FileParser.new
+      parser.load_listed
+    end
+
+    def load_listing(list_type)
+      people_list = self.load_files
+      valid_call = people_list.first.respond_to?(list_type)
+      SortList.load_by(list_type, people_list) if valid_call
+    end
   end
 end
 
--- lib/record_list/terminal_record.rb
+++ lib/record_list/terminal_record.rb
@@ -1,17 +1,13 @@
 class TerminalRecord < Record
+  class << self
 
-  def self.load_listing(list_type)
-    people_list = self.load_files
+    def run
+      TerminalDisplay.list_type_prompt
+      list_type = gets.chomp.downcase
 
-    SortList.load_by(list_type, people_list) if people_list.first.respond_to?(list_type)
-  end
+      sorted_list = self.load_listing(list_type)
 
-  def self.run
-    TerminalDisplay.list_type_prompt
-    list_type = gets.chomp.downcase
-
-    sorted_list = self.load_listing(list_type)
-
-    sorted_list ? TerminalDisplay.results(sorted_list) : TerminalDisplay.entry_error
+      sorted_list ? TerminalDisplay.results(sorted_list) : TerminalDisplay.entry_error
+    end
   end
 end
